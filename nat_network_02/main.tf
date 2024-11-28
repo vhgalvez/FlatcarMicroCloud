860e38a0efd49ae4318c9bc6da6989703d57b971
Victor Galvez <vhgalvez@gmail.com>
2024-11-28 11:10:14 +0100

    Update main.tf

--- nat_network_02/main.tf
+++ nat_network_02/main.tf
@@ -7,10 +7,6 @@ terraform {
       source  = "dmacvicar/libvirt"
       version = "0.8.1"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
   }
 }
 
@@ -18,88 +14,40 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Crear la red NAT
-resource "libvirt_network" "kube_network_02" {
-  name      = var.rocky9_network_name
-  mode      = "nat"
-  autostart = true
-  addresses = ["10.17.3.0/24"]
+# Crea el directorio para el pool si no existe
+resource "null_resource" "create_pool_directory" {
+  provisioner "local-exec" {
+    command = "sudo mkdir -p /mnt/lv_data/organized_storage/volumes/${var.cluster_name}_bastion && sudo chown qemu:qemu /mnt/lv_data/organized_storage/volumes/${var.cluster_name}_bastion"
+  }
 }
 
-# Crear el pool de almacenamiento
-resource "libvirt_pool" "volumetmp_nat_02" {
-  name = "${var.cluster_name}_nat_02"
+# Define el pool después de crear el directorio
+resource "libvirt_pool" "volumetmp_bastion" {
+  depends_on = [null_resource.create_pool_directory]
+
+  name = "${var.cluster_name}_bastion"
   type = "dir"
 
   target {
-    path = "/mnt/lv_data/organized_storage/volumes/${var.cluster_name}_nat_02"
+    path = "/mnt/lv_data/organized_storage/volumes/${var.cluster_name}_bastion"
   }
 }
 
-# Crear el volumen base de la imagen
+# Define el volumen de la imagen base
 resource "libvirt_volume" "rocky9_image" {
-  depends_on = [libvirt_pool.volumetmp_nat_02]
+  depends_on = [libvirt_pool.volumetmp_bastion]
 
   name   = "${var.cluster_name}_rocky9_image"
   source = var.rocky9_image
-  pool   = libvirt_pool.volumetmp_nat_02.name
+  pool   = libvirt_pool.volumetmp_bastion.name
   format = "qcow2"
 }
 
-# Crear disco adicional para storage1 (si aplica)
-resource "libvirt_volume" "additional_disk_rc_storage1" {
-  depends_on = [libvirt_pool.volumetmp_nat_02]
-
-  name   = var.additional_disk_rc_storage1.name
-  pool   = libvirt_pool.volumetmp_nat_02.name
-  format = var.additional_disk_rc_storage1.format
-  size   = var.additional_disk_rc_storage1.size
-}
-
-# Configurar los datos de Cloud-Init
-data "template_file" "vm_configs" {
+# Define las máquinas virtuales
+resource "libvirt_domain" "vm" {
   for_each = var.vm_rockylinux_definitions
 
-  template = file("${path.module}/config/${each.key}-user-data.tpl")
-  vars = {
-    ssh_keys       = jsonencode(var.ssh_keys),
-    hostname       = each.value.hostname,
-    short_hostname = each.value.short_hostname,
-    timezone       = var.timezone,
-    ip             = each.value.ip,
-    gateway        = var.gateway,
-    dns1           = var.dns1,
-    dns2           = var.dns2
-  }
-}
-
-# Crear disco de Cloud-Init
-resource "libvirt_cloudinit_disk" "vm_cloudinit" {
-  for_each = var.vm_rockylinux_definitions
-
-  name      = "${each.key}_cloudinit.iso"
-  pool      = libvirt_pool.volumetmp_nat_02.name
-  user_data = data.template_file.vm_configs[each.key].rendered
-}
-
-# Crear los discos de las máquinas virtuales
-resource "libvirt_volume" "vm_disk" {
-  for_each = var.vm_rockylinux_definitions
-
-  depends_on = [libvirt_volume.rocky9_image]
-
-  name           = "${each.key}-${var.cluster_name}.qcow2"
-  base_volume_id = libvirt_volume.rocky9_image.id
-  pool           = libvirt_pool.volumetmp_nat_02.name
-  format         = "qcow2"
-  size           = max(each.value.volume_size * 1024 * 1024 * 1024, libvirt_volume.rocky9_image.size)
-}
-
-# Crear las máquinas virtuales
-resource "libvirt_domain" "vm_nat_02" {
-  for_each = var.vm_rockylinux_definitions
-
-  name   = each.key
+  name   = each.value.hostname
   memory = each.value.memory
   vcpu   = each.value.cpus
 
@@ -120,13 +68,21 @@ resource "libvirt_domain" "vm_nat_02" {
     listen_type = "address"
   }
 
-  cpu {
-    mode = "host-passthrough"
-  }
-
   console {
     type        = "pty"
     target_type = "serial"
     target_port = "0"
   }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+}
+
+# Define la red
+resource "libvirt_network" "kube_network_02" {
+  name      = "kube_network_02"
+  mode      = "nat"
+  autostart = true
+  addresses = ["10.17.3.0/24"]
 }
