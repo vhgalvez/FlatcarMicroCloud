cc2cfc17ca25081463266cc7aa5e4b277453560c
Victor Galvez <vhgalvez@gmail.com>
2024-12-18 23:59:08 +0100

    Update main.tf

--- pfsense/main.tf
+++ pfsense/main.tf
@@ -1,6 +1,7 @@
 # pfsense\main.tf
 terraform {
   required_version = ">= 1.4.0"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
@@ -13,18 +14,21 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+# Red WAN
 resource "libvirt_network" "wan" {
   name   = "wan_network"
   mode   = "bridge"
   bridge = "br0"
 }
 
+# Red LAN
 resource "libvirt_network" "lan" {
   name   = "lan_network"
   mode   = "bridge"
   bridge = "br1"
 }
 
+# Pool de almacenamiento
 resource "libvirt_pool" "pfsense_pool" {
   name = "pfsense_storage"
   type = "dir"
@@ -33,6 +37,7 @@ resource "libvirt_pool" "pfsense_pool" {
   }
 }
 
+# ISO de pfSense
 resource "libvirt_volume" "pfsense_iso" {
   name   = "pfsense_installer.iso"
   pool   = libvirt_pool.pfsense_pool.name
@@ -40,6 +45,7 @@ resource "libvirt_volume" "pfsense_iso" {
   format = "iso"
 }
 
+# Disco principal
 resource "libvirt_volume" "pfsense_disk" {
   name   = "pfsense_disk.qcow2"
   pool   = libvirt_pool.pfsense_pool.name
@@ -47,11 +53,13 @@ resource "libvirt_volume" "pfsense_disk" {
   size   = var.pfsense_vm_config.disk_size_gb * 1024 * 1024 * 1024
 }
 
+# Máquina Virtual pfSense
 resource "libvirt_domain" "pfsense" {
   name   = "pfsense-firewall"
   memory = var.pfsense_vm_config.memory
   vcpu   = var.pfsense_vm_config.cpus
 
+  # Interfaces de Red
   network_interface {
     network_id = libvirt_network.wan.id
     mac        = var.pfsense_vm_config.wan_mac
@@ -62,25 +70,32 @@ resource "libvirt_domain" "pfsense" {
     mac        = var.pfsense_vm_config.lan_mac
   }
 
+  # Disco principal
   disk {
     volume_id = libvirt_volume.pfsense_disk.id
   }
 
+  # Disco ISO como CD-ROM
   disk {
     volume_id = libvirt_volume.pfsense_iso.id
-    scsi      = false
+    target_bus = "ide"
+    target_device = "hdc"  # Forzar como CD-ROM
+    readonly  = true
   }
 
+  # Orden de arranque (CD-ROM primero)
   boot_device {
-    dev = var.pfsense_boot_order
+    dev = ["cdrom", "hd"]
   }
 
+  # Gráficos VNC
   graphics {
     type        = "vnc"
     listen_type = "address"
     autoport    = true
   }
 
+  # Consola Serial
   console {
     type        = "pty"
     target_type = "serial"
