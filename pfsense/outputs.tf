6dece97b5ee2d893e64bddba86eb4d6e896ae2f2
Victor Galvez <vhgalvez@gmail.com>
2024-12-19 14:58:51 +0100

    update

--- pfsense/outputs.tf
+++ pfsense/outputs.tf
@@ -1,15 +1,8 @@
 # pfsense\outputs.tf
-output "wan_ip" {
-  description = "WAN IP for pfSense"
-  value       = var.pfsense_vm_config.wan_ip
+output "pfsense_vm_name" {
+  value = libvirt_domain.pfsense.name
 }
 
-output "lan_ip" {
-  description = "LAN IP for pfSense"
-  value       = var.pfsense_vm_config.lan_ip
-}
-
-output "vnc_access" {
-  description = "VNC Access for pfSense"
-  value       = "Conéctate a la VM mediante VNC en el puerto asignado automáticamente."
+output "pfsense_vnc_connection" {
+  value = "VNC connection details: Connect to localhost on the assigned port."
 }
