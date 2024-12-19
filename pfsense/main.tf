f03367ced779b295f2ea7e7e9f458384b7323924
Victor Galvez <vhgalvez@gmail.com>
2024-12-19 04:52:17 +0100

    Update main.tf

--- pfsense/main.tf
+++ pfsense/main.tf
@@ -77,6 +77,12 @@ resource "libvirt_domain" "pfsense" {
   # Disco ISO como CD-ROM
   disk {
     volume_id = libvirt_volume.pfsense_iso.id
+    scsi      = false
+    target {
+      dev = "hdc"
+      bus = "ide"
+    }
+    readonly = true
   }
 
   # Orden de arranque
