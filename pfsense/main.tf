5e1a6eb3d7e7da69349143835872107e0bf0aa21
Victor Galvez <vhgalvez@gmail.com>
2024-12-21 06:11:22 +0100

    update maint

--- pfsense/main.tf
+++ pfsense/main.tf
@@ -12,6 +12,7 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+# Definición del pool de almacenamiento
 resource "libvirt_pool" "pfsense_pool" {
   name = "pfsense-pool"
   type = "dir"
@@ -20,6 +21,7 @@ resource "libvirt_pool" "pfsense_pool" {
   }
 }
 
+# Creación del volumen para la VM
 resource "libvirt_volume" "pfsense_disk" {
   name   = "pfsense.qcow2"
   pool   = libvirt_pool.pfsense_pool.name
@@ -27,34 +29,43 @@ resource "libvirt_volume" "pfsense_disk" {
   format = "qcow2"
 }
 
+# Configuración de la máquina virtual
 resource "libvirt_domain" "pfsense_vm" {
   name   = var.pfsense_vm_name
   memory = var.pfsense_vm_config.memory
   vcpu   = var.pfsense_vm_config.cpus
 
+  # Disco
   disk {
     volume_id = libvirt_volume.pfsense_disk.id
   }
 
+  # Interfaz WAN
   network_interface {
     bridge = "br0" # WAN
+    model  = "virtio"
   }
 
+  # Interfaz LAN
   network_interface {
     bridge = "br1" # LAN
+    model  = "virtio"
   }
 
+  # Configuración del dispositivo de arranque
   boot_device {
     dev = ["hd"]
   }
 
+  # Configuración de gráficos (opcional)
   graphics {
     type           = "vnc"
     listen_address = "0.0.0.0"
     listen_type    = "address"
   }
 }
-# Salidas de las direcciones IP
+
+# Salidas para las direcciones IP de WAN y LAN
 output "pfSense_WAN_IP" {
   value = "http://${var.wan_ip}:80"
 }
