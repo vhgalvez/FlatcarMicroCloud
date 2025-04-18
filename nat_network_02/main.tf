547d0f1b776eba1f10b412a1f1258430d652d14b
Victor Galvez <vhgalvez@gmail.com>
2025-04-18 19:48:43 +0200

    Update main.tf

--- nat_network_02/main.tf
+++ nat_network_02/main.tf
@@ -93,7 +93,15 @@ resource "libvirt_domain" "vm_nat_02" {
 
   disk {
     volume_id = libvirt_volume.vm_disk[each.key].id
-    # Removed unsupported 'bus' argument
+    bus       = "virtio" # Ensure virtio is used for the main disk
+  }
+
+  dynamic "disk" {
+    for_each = { for k, v in var.additional_disks : k => v if startswith(k, "${each.key}-") }
+    content {
+      volume_id = disk.value.id
+      bus       = "virtio" # Ensure virtio is used for additional disks
+    }
   }
 
   graphics {
