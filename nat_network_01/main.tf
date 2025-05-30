7c5d58e835677e0e739a50fc6a54ac0344986fa7
Victor Galvez <vhgalvez@gmail.com>
2025-05-30 02:38:56 +0200

    Update main.tf

--- nat_network_01/main.tf
+++ nat_network_01/main.tf
@@ -56,9 +56,9 @@ data "template_file" "vm_configs" {
 resource "libvirt_cloudinit_disk" "vm_cloudinit" {
   for_each = var.vm_linux_definitions
 
-  name      = "${each.key}_cloudinit.iso"
-  pool      = libvirt_pool.volumetmp.name
-  user_data = data.template_file.vm_configs[each.key].rendered
+  name           = "${each.key}_cloudinit.iso"
+  pool           = libvirt_pool.volumetmp.name
+  user_data      = data.template_file.vm_configs[each.key].rendered
   network_config = templatefile("${path.module}/config/network-config.tpl", {
     ip      = each.value.ip
     gateway = each.value.gateway
