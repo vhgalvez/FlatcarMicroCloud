319904f699e803099a7b26074045a30327d2cc33
Victor Galvez <vhgalvez@gmail.com>
2025-06-01 00:26:56 +0200

    Update main.tf

--- br0_network_01/main.tf
+++ br0_network_01/main.tf
@@ -92,8 +92,9 @@ resource "libvirt_domain" "vm" {
   vcpu   = each.value.cpus
 
   network_interface {
-    bridge    = "br0"
-    addresses = [each.value.ip]
+    network_id = libvirt_network.br0.id
+    bridge     = "br0"
+    addresses  = [each.value.ip]
   }
 
   disk {
