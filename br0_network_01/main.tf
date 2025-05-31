7efcbdad24e75c484c415947114ea04c8b2268e0
Victor Galvez <vhgalvez@gmail.com>
2025-05-31 13:53:14 +0200

    Update main.tf

--- br0_network_01/main.tf
+++ br0_network_01/main.tf
@@ -37,7 +37,7 @@ resource "libvirt_volume" "so_image" {
   format = "qcow2"
 }
 
-data "template_file" "vm_configs" {
+data "template_file" "vm-configs" {
   for_each = var.vm_linux_definitions
 
   template = file("${path.module}/config/${each.key}-user-data.tpl")
@@ -47,20 +47,20 @@ data "template_file" "vm_configs" {
     short_hostname = each.value.short_hostname
     timezone       = var.timezone
     ip             = each.value.ip
-    gateway        = each.value.gateway
-    dns1           = each.value.dns1
-    dns2           = each.value.dns2
+    gateway        = var.gateway
+    dns1           = var.dns1
+    dns2           = var.dns2
     cluster_domain = var.cluster_domain
-    cluster_name  = var.cluster_name
   }
 }
+
 
 resource "libvirt_cloudinit_disk" "vm_cloudinit" {
   for_each = var.vm_linux_definitions
 
-  name           = "${each.key}_cloudinit.iso"
-  pool           = libvirt_pool.volumetmp.name
-  user_data      = data.template_file.vm_configs[each.key].rendered
+  name      = "${each.key}_cloudinit.iso"
+  pool      = libvirt_pool.volumetmp.name
+  user_data = data.template_file.vm_configs[each.key].rendered
   network_config = templatefile("${path.module}/config/network-config.tpl", {
     ip      = each.value.ip
     gateway = each.value.gateway
